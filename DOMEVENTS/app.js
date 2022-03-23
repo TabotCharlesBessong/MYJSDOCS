574c478bb4b261b5e12b8a2c74a5f47902bcf930
TabotCharlesBessong <ebezebeatrice@gmail.com>
2022-03-23 10:05:28 +0100

    creating element with javascrupt dom element

---
+++ DOMEVENTS/app.js
@@ -0,0 +1,12 @@
+
+// const body = document.body
+// console.log(body);
+// alert('Hello world')
+
+let divElement = document.createElement('div')
+let inputElement = document.createElement('input')
+
+document.body.appendChild(divElement)
+divElement.appendChild(inputElement)
+
+console.dir(divElement);

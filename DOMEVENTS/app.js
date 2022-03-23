ad5b02fc061fafc769fbbcac3af1495729e8d00a
TabotCharlesBessong <ebezebeatrice@gmail.com>
2022-03-23 10:32:09 +0100

    innerhtml

--- DOMEVENTS/app.js
+++ DOMEVENTS/app.js
@@ -16,3 +16,16 @@ console.dir(divElement);
 document.querySelector("#text").setAttribute('value','Charles')
 divElement.setAttribute('class','division')
 console.log(divElement);
+
+// Text content
+const text2 = document.querySelector('#text2')
+const text1 = document.querySelector('#text1')
+text2.textContent = "Hello my love you have been missing , where have you been ?"
+
+
+//  The difference between text content  and innerHtml is the the fomer adss more text and the later replaces the  text 
+
+text1.innerHTML =  "<b>Hello men</b>";
+
+alert(text2.textContent)
+alert(text1.textContent)

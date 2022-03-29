bf90aa287800bbefe829c32efc48a6e2ef96da78
TabotCharlesBessong <ebezebeatrice@gmail.com>
2022-03-30 00:53:55 +0100

    unique values

---
+++ 02-unique-values/app.js
@@ -0,0 +1,42 @@
+// Unique Values
+const menu = [
+  {
+    name: 'pancakes',
+    category: 'breakfast',
+  },
+  {
+    name: 'burger',
+    category: 'lunch',
+  },
+  {
+    name: 'steak',
+    category: 'dinner',
+  },
+  {
+    name: 'bacon',
+    category: 'breakfast',
+  },
+  {
+    name: 'eggs',
+    category: 'breakfast',
+  },
+  {
+    name: 'pasta',
+    category: 'dinner',
+  },
+];
+// const categories = ['all', ...new Set(menu.map((item) => item.category))];
+// map - get all instances
+// new Set - narrow down
+// ['all',...] - turn it back to array
+
+// we use the set datatype as it return only uniqua values
+
+const categories = ['all', ...new Set(menu.map((item) => item.category))];
+// console.log(categories);
+const result = document.querySelector('.result');
+result.innerHTML = categories
+  .map((category) => {
+    return `<button>${category}</button>`;
+  })
+  .join('');
